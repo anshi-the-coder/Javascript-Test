69ee4c5aeb3ae0cfd2ae2ca5161582ca0e30bd52
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-08 10:35:56 +0530

    sum of a list by using math class

---
+++ math.js
@@ -0,0 +1,47 @@
+class Math{
+    constructor(params){
+        this.params=params
+    }
+    sum(){
+        if(Array.isArray(this.params)){
+           return this.params.reduce((a,b)=>(a+b)) 
+        }else{
+            return "This is not an array"
+        }
+    }
+    min(){
+        if(Array.isArray(this.params)){
+            let min=this.params[0]
+            for(let i=1;i<this.params.length;i++){
+                if(this.params[i]<min){
+                    min=this.params[i]
+                }
+            }
+            return min
+         }else{
+             return "This is not an array"
+         }
+    }
+
+    max(){
+        if(Array.isArray(this.params)){
+            let max=this.params[0]
+            for(let i=1;i<this.params.length;i++){
+                if(this.params[i]>max){
+                    max=this.params[i]
+                }
+            }
+            return max
+         }else{
+             return "This is not an array"
+         }
+    }
+}
+
+let math=new Math([8,9,7,76,89])
+let sum1=math.sum()
+console.log(sum1)
+let min1=math.min()
+console.log(min1)
+let max1=math.max()
+console.log(max1)
