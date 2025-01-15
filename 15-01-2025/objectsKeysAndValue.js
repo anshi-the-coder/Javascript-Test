133358745d789da566ebe3e1a0a05f695655c97b
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-15 14:46:56 +0530

    Objects keys and value

---
+++ 15-01-2025/objectsKeysAndValue.js
@@ -0,0 +1,15 @@
+let obj={
+    brand: 'Toyota',
+    model: 'Corolla',
+    year: 2020
+}
+for(let key in obj){
+    console.log(` ${key}: ${obj[key]}`)
+}
+ 
+
+
+// output
+// brand: Toyota
+//  model: Corolla
+//  year: 2020
