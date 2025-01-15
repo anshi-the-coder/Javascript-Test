a2bdc779f3596e8b7c1b8a8b1d4815831ce7226b
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-15 14:35:26 +0530

    use of foIn loop

---
+++ 15-01-2025/forIn.js
@@ -0,0 +1,16 @@
+let obj={
+    name: 'John',
+    age: 30,
+    city: 'New York'
+}
+for(let key in obj){
+   // console.log(obj[key])
+   console.log(` ${key}:  ${obj[key]}`)
+}
+
+
+
+// output
+// name: John
+// age: 30
+// city: New York
