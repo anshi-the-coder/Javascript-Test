91a500e3784cd3b8ee9bed5732ee62ae7274247c
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-24 11:57:51 +0530

    Java Script Dates

---
+++ Basic programs of js/javaScript.js
@@ -0,0 +1,43 @@
+function getDayName(dateString) {
+    let date = new Date(dateString);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayIndex = date.getDay();
+
+       return days[dayIndex];
+}function getDayName(dateString) {
+    let date = new Date(dateString);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayIndex = date.getDay();
+    return days[dayIndex];
+}
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}function getDayName(dateString) {
+    let date = new Date(dateString);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayIndex = date.getDay();
+
+       return days[dayIndex];
+}function getDayName(dateString) {
+    let date = new Date(dateString);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayIndex = date.getDay();
+    return days[dayIndex];
+}
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
