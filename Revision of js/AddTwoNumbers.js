8c3223647e8f0c505d5b91f4a27337261f5d0d3e
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-16 13:14:42 +0530

    Revision of add/sub two numbers

---
+++ Revision of js/AddTwoNumbers.js
@@ -0,0 +1,10 @@
+let addTwoNumbers= (a,b)=>{
+    return a+b
+}
+let sum= addTwoNumbers(5,3)
+console.log(sum)
+let subtractTwoNumbers=(a,b)=>{
+    return a-b
+}
+let sub=  subtractTwoNumbers(8,3)
+console.log(sub)
